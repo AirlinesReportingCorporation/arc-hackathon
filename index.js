7321c79bca70aba8fd1c195ed3e77097d41cadf3
Ian Fajardo <ianfajardozx@gmail.com>
2019-06-23 07:05:47 -0400

    fix img static

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/plugin", express.static(__dirname + '/plugin'));
 app.use("/lib", express.static(__dirname + '/lib'));
-app.use("/img", express.static(__dirname + '/lib'));
+app.use("/img", express.static(__dirname + '/img'));
 
 
 app.engine('handlebars', hbs.engine);
